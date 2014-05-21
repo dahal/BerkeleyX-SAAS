7c4becdaba4bf0bfbe3f09fe1d9d8211fe6f27b3
Puru Dahal <pdahal@me.com>
2014-05-21 16:29:53 -0500

    Done :white_check_mark:

--- HW1/ruby-calisthenics/fun_with_strings/fun_with_strings.rb
+++ HW1/ruby-calisthenics/fun_with_strings/fun_with_strings.rb
@@ -1,13 +1,28 @@
 module FunWithStrings
   def palindrome?
     # your code here
+    string = self.gsub(/\W/, '').downcase
+    #string = self.gsub(/[^(a-zA-Z)]/){|x| ""}.downcase
+    string == string.reverse
   end
+
+
   def count_words
-    # your code here
+    array = self.gsub(/\W/, ' ').downcase.split
+    #array = self.gsub(/[^(a-zA-Z)]/){|x| " "}.downcase.split
+    hash = Hash.new(0)
+    array.each do |a|
+      hash[a] += 1 
+    end
+    return hash
   end
+
   def anagram_groups
-    # your code here
+    array = self.downcase.split
+    hash = array.group_by{|x| x.chars.sort}
+    hash.select{|v| v.size > 1}.values
   end
+
 end
 
 # make all the above functions available as instance methods on Strings:
